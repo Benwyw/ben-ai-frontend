e62e00f640e395b2b4a6c16b88e71701160e978d
Ben <57297093+Benwyw@users.noreply.github.com>
2022-11-12 17:24:14 +0800

    move to components sub dir

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-import MovieCard from './MovieCard';
+import MovieCard from './components/MovieCard';
 
 import './App.css';
 import SearchIcon from './search.svg';
